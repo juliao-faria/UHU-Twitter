e0d4fc4de1065ec3d3fedad67728e4215ccb645d
Juliao <juliaofariamaneco@gmail.com>
2023-01-11 14:40:06 +0100

    eeee
    eee

--- front-end/src/components/Estatistica/Estatisticas.jsx
+++ front-end/src/components/Estatistica/Estatisticas.jsx
@@ -1,25 +1,86 @@
 import Card from "../UI/Card";
-import classes from "./Estatistica.module.css";
+import "./Estatistica.css";
 const Estatistica = () => {
 	return (
 		<div className="container">
-			<div className="row">
+			<br />
+			<br />
+
 				<Card>
-					<ul>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-						<li className={classes.style}>hshshshshhsh</li>
-					</ul>
+					<table className="table">
+						
+						<tbody>
+							<tr>
+								
+								<td>Fecha de descarga</td>
+								<td>Desde 2022-12-01 has 2022-12-02</td>
+							</tr>
+							<tr>
+							
+								<td>Tweet más retuiteado.</td>
+								<td>La vida de Leonel Messi en el football</td>
+							</tr>
+							<tr>
+								
+								<td>Url más popular</td>
+								<td>https://moodle.uhu.es - <span>Numero de veces 6</span></td>
+							</tr>
+							<tr>
+								
+								<td>Hashtags más frecuentes</td>
+								<td>#ahhahaahahhahah - <span>Numero de veces 17</span></td>
+							</tr><tr>
+								
+								<td>Usuarios más mencionados</td>
+								<td>Carlos Argentino - <span>Numero de veces 12</span></td>
+							</tr>
+						</tbody>
+						
+					</table>
+				</Card>
+
+
+{/*-------------------------------------------------------------------------usuarios con mas tweets publicados --------------------------------------- */}
+				<Card>
+					<table className="table">
+						<thead>
+							<tr>
+								
+								<th scope="col">Usuario con más tweets</th>
+								<th scope="col">Cantidad de tweets</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								
+								<td>Fecha de descarga</td>
+								<td>Desde 2022-12-01 has 2022-12-02</td>
+							</tr>
+							<tr>
+							
+								<td>Tweet más retuiteado.</td>
+								<td>La vida de Leonel Messi en el football</td>
+							</tr>
+							<tr>
+								
+								<td>Url más popular</td>
+								<td>https://moodle.uhu.es - <span>Numero de veces 6</span></td>
+							</tr>
+							<tr>
+								
+								<td>Hashtags más frecuentes</td>
+								<td>#ahhahaahahhahah - <span>Numero de veces 17</span></td>
+							</tr><tr>
+								
+								<td>Usuarios más mencionados</td>
+								<td>Carlos Argentino - <span>Numero de veces 12</span></td>
+							</tr>
+						</tbody>
+						
+					</table>
 				</Card>
 			</div>
-		</div>
+		
 	);
 };
 
