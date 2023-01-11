7e64392f1d51b2113c533822710b3d9e1bb59ebe
Juliao <juliaofariamaneco@gmail.com>
2023-01-11 11:42:15 +0100

    jjjj
    ca,bios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Twitter from "./components/Twitters/Twitter";
 import NavBar from "./components/UI/NavBar";
 
+
 const App = () => {
     return (
        <div>
--- front-end/src/components/UI/Card.js
+++ front-end/src/components/UI/Card.js
@@ -2,8 +2,11 @@ import React from "react";
 import classes from "./Card.module.css"
 
 const Card = (props) => {
-  return(
-    <div className={classes.card}>{props.children}</div>
+  return (
+    <div className="col-lg-12">
+      <div className={classes.card}>{props.children}</div>
+    </div>
+
   )
 }
 export default Card;
